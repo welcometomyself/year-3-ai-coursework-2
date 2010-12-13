1b022a6a152f240981c82178108e354b2557ac23
j.chlanda <j.chlanda@ad675049-337c-877d-8f9d-20bcf3b7de3e>
2010-12-13 23:59:04 +0000

    Class stubs created. Linked with google code.

---
+++  year-3-ai-coursework-2 --username j.chlanda/tsp/src/AStar.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author jakub
+ *
+ */
+public class AStar {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
